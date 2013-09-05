4d171d110878d1f28fc55660a16ed1b9dd20edd5
Ryan Ramage <ryan.ramage@gmail.com>
2013-09-05 11:43:54 -0600

    fixes #46. Include the db name in the backup file

--- static/js/settings.js
+++ static/js/settings.js
@@ -196,7 +196,7 @@ $(function(){
                       var file = new Blob([JSON.stringify(ddoc.app_settings, null, 4)], {"type": "application\/json"});
 
                       var timestamp = moment().format('YYYY-MM-DD-HHmmss');
-                      a.download = doc.doc_id + '_' + timestamp + '_settings.json';
+                      a.download = doc.doc_id + '_' +  doc.installed.db + '_' + timestamp + '_settings.json';
                       a.href = URL.createObjectURL(file);
 
                    });
